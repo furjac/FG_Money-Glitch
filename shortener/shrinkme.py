c4030bc2197b221fdb237ca13c05e642f824699e
Furjack <81816200+furjac@users.noreply.github.com>
2024-10-29 22:27:11 +0530

    Fixed a major issue with audio file
    no more issue with audio overwritten issue and fixed some wierd issues with shrinkme and also make sure use lesser and lesser threads for shrinkme or else its not working

--- shortener/shrinkme.py
+++ shortener/shrinkme.py
@@ -7,7 +7,8 @@ import requests
 import time
 import random
 from wit import Wit
-
+import uuid
+import os
 
 def main(pluginfile, proxy_plugin, headless, num1, num2, bnum1, bnum2, r_links, ouo_links,wit_key):
     def get_random_link():
@@ -48,6 +49,12 @@ def main(pluginfile, proxy_plugin, headless, num1, num2, bnum1, bnum2, r_links, 
     options.add_experimental_option('excludeSwitches', ['enable-logging', "enable-automation"])
     # options.add_experimental_option("detach", True)
     options.add_extension(proxy_plugin)
+    prefs = {
+        "download_restrictions": 3,
+    }
+    options.add_experimental_option(
+        "prefs", prefs
+    )
     driver = webdriver.Chrome(options=options)
     driver.execute_cdp_cmd("Page.enable", {})
     driver.execute_cdp_cmd("Network.enable", {})
@@ -63,28 +70,41 @@ def main(pluginfile, proxy_plugin, headless, num1, num2, bnum1, bnum2, r_links, 
     driver.get(random_redirects())
     time.sleep(5)
     driver.get(get_random_link())
+    time.sleep(5)
     time.sleep(random_sleep(num1, num2))
     WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(
         (By.CSS_SELECTOR, ".g-recaptcha > div:nth-child(1) > div:nth-child(1) > iframe:nth-child(1)")))
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "span#recaptcha-anchor"))).click()
     driver.switch_to.default_content()
     time.sleep(random_sleep(7,9))
-    WebDriverWait(driver, 10).until(
-        EC.frame_to_be_available_and_switch_to_it((By.XPATH, "/html/body/div[3]/div[4]/iframe")))
-    WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[3]/div[2]/div[1]/div[1]/div[2]/button"))).click()
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.frame_to_be_available_and_switch_to_it((By.XPATH, "/html/body/div[3]/div[4]/iframe")))
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/div[3]/div[2]/div[1]/div[1]/div[2]/button"))).click()
+    except:
+        pass
+    
     while True:
         # Use try to check if the element exists or not
         try:
             # Get audio's link
-            t = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'rc-audiochallenge-tdownload')))
-            t = t.find_element(By.TAG_NAME, 'a')
-            t = t.get_attribute('href')
+            try:
+                t = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, 'rc-audiochallenge-tdownload')))
+                t = t.find_element(By.TAG_NAME, 'a')
+                t = t.get_attribute('href')
+            except:
+                t = WebDriverWait(driver, 20).until(
+                    EC.presence_of_element_located((By.ID, 'audio-source'))
+                )
+                t = t.get_attribute('src')
+            unique_id = str(uuid.uuid4())
+            audio = f'audio_{unique_id}.mp3'
 
             # Download audio file
             r = requests.get(t, allow_redirects=True)
-            open('audio.mp3', 'wb').write(r.content)
+            open(audio, 'wb').write(r.content)
 
             def transcribe_audio_to_text(audio_file_path, wit_token):
                 client = Wit(access_token=wit_token)
@@ -94,11 +114,12 @@ def main(pluginfile, proxy_plugin, headless, num1, num2, bnum1, bnum2, r_links, 
 
                 return response.get('text', '')
 
-            transcription = transcribe_audio_to_text('audio.mp3', wit_key)
+            transcription = transcribe_audio_to_text(audio, wit_key)
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'audio-response'))).send_keys(
                 transcription)
             WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'verify-button-holder'))).click()
-        except:
+            os.remove(audio)
+        except Exception as e:
             # If the app doesn't find the element, it means we are done!
             break
 
@@ -109,9 +130,10 @@ def main(pluginfile, proxy_plugin, headless, num1, num2, bnum1, bnum2, r_links, 
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="btn2"]'))).click()
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="tp-snp2"]'))).click()
     time.sleep(15)
-    WebDriverWait(driver, 10).until(
+    button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, '.btn.btn-success.btn-lg.get-link'))
-    ).click()
+    )
+    driver.execute_script("arguments[0].click();", button)
 
     time.sleep(bef_quit(bnum1, bnum2))
     time.sleep(3)
